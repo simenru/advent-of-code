d7d1aac1d6d6942d7ac9e26dee9258f949d82d13
Simen Ellingsen Rustad <simen.rustad@gmail.com>
2024-12-06 08:20:07 +0100

    Improved solution to 06b

--- 2024/06b.py
+++ 2024/06b.py
@@ -11,22 +11,38 @@ for y, line in enumerate(map):
 
 map = [[1 if c == "#" else 0 for c in line] for line in map]
 
+def step(map, px, py, direction) -> tuple[int, int, str]:
+    match direction:
+        case "N":
+            if py > 0 and map[py-1][px] == 1:
+                return px, py, "E"
+            return px, py-1, direction
+        case "E":
+            if px+1 < len(map[0]) and map[py][px+1] == 1:
+                return px, py, "S"
+            return px+1, py, direction
+        case "S":
+            if py+1 < len(map) and map[py+1][px] == 1:
+                return px, py, "W"
+            return px, py+1, "S"
+        case "W":
+            if px > 0 and map[py][px-1] == 1:
+                return px, py, "N"
+            return px-1, py, "W"
 
-class COLORS:
-    yellow = "\033[93m"
-    red = "\033[91m"
-    end = "\033[0m"
-
-def print_map(map, px, py):
-    print("----")
-    for y, line in enumerate(map):
-        line = "".join([str(c) for c in line])
-        line = line.replace("0", ".")
-        line = line.replace("1", "#")
-
-        if y == py:
-            line = line[:x] + COLORS.red + "X" + COLORS.end + line[x+1:]
-        print(line)
+def visited_cells(map, sx, sy, sdir) -> set[tuple[int, int]]:
+    px, py, direction = sx, sy, sdir
+    visited = set()
+    while True:
+        if px < 0 or px >= len(map[0]):
+            break
+        if py < 0 or py >= len(map):
+            break
+        
+        px, py, direction = step(map, px, py, direction)
+        visited.add((px, py))
+    return visited
+    
 
 def is_loop(map, sx, sy, sdir):
     px, py = sx, sy
@@ -40,41 +56,21 @@ def is_loop(map, sx, sy, sdir):
         if py < 0 or py >= len(map):
             return False
         
-        match direction:
-            case "N":
-                if py > 0 and map[py-1][px] == 1:
-                    direction = "E"
-                    continue
-                py -= 1
-            case "E":
-                if px+1 < len(map[0]) and map[py][px+1] == 1:
-                    direction = "S"
-                    continue
-                px += 1
-            case "S":
-                if py+1 < len(map) and map[py+1][px] == 1:
-                    direction  = "W"
-                    continue
-                py += 1
-            case "W":
-                if px > 0 and map[py][px-1] == 1:
-                    direction = "N"
-                    continue
-                px -= 1
+        px, py, direction = step(map, px, py, direction)
         
         if f"{px},{py},{direction}" in visited:
             return True
 
 count = 0
-for y in range(len(map)):
-    print(y)
-    for x in range(len(map[0])):
-        map_copy = [line.copy() for line in map]
-        if x != sx or y != sy and map[y][x] != 1:
-            map_copy[y][x] = 1
-            if is_loop(map_copy, sx, sy, "N"):
-                #print(f" {x}, {y}")
-                count += 1
-                print(f" {count}")
-                #print_map(map_copy, sx, sy)
+for x, y in visited_cells(map, sx, sy, "N"):
+    if x < 0 or x >= len(map[0]):
+        continue
+    if y < 0 or y >= len(map):
+        continue
+    map_copy = [line.copy() for line in map]
+    if x != sx or y != sy and map[y][x] != 1:
+        map_copy[y][x] = 1
+        if is_loop(map_copy, sx, sy, "N"):
+            count += 1
+
 print(count)
